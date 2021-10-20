5d039afb58e266dd9f264f0ffa72eeb7a8f3ce21
FawziaAlushrafii <Fawzia.Hassan.ruh.k@tuwaiq.edu.sa>
2021-10-20 03:38:22 +0300

    Initial commit

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,22 @@
+fun main(args: Array<String>) {
+    var stars =4
+    for (f in 1 ..stars){
+        for (m in 1 ..f){
+            print("*")
+    }
+    println()
+}
+    //
+    val numbers=5
+        for ( t in 1 ..5) {
+            for (h in 1..t) {
+                for(t in 1..5){
+                    val process =numbers*t
+                    println("$numbers *$t =$process")
+                    }
+                print("$h")
+            }
+                println()
+
+            }
+        }
